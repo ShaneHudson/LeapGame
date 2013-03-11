56b0dd497e9ebc7af44b1c7745587f9d9603c4c8
Chris Ewbank <djpc123@gmail.com>
2013-03-11 17:18:58 +0000

    add the 'river'

--- js/game.js
+++ js/game.js
@@ -1,20 +1,31 @@
 var game = document.querySelector('#game');
 var ctx = game.getContext('2d');
-var x = y = 10;
-game.height = width = window.innerHeight; 
-game.width = height = window.innerWidth;
+var y = 10;
+var x = (window.innerWidth + 30);
+game.height = height = (window.innerHeight - 30); 
+game.width = width = window.innerWidth;
+var y = 10;
+var x = (width + 100);
+var x2 = (width + 10);
+var y2 = (height - 60);
+
 drawThings();
 
 function drawThings() {
-	ctx.clearRect(x, y, 50, 50);
-	++x;
-	++y;
-	ctx.fillStyle = "#ff0000";
+	ctx.clearRect(0, 0, width, height);
+	--x;
+	--x2;
+	if ((y2 - y) < 0 ) {
+		ctx.fillStyle = "#0000CC";
+	} else {
+		ctx.fillStyle = "#ff0000";
+	}
 	ctx.fillRect(x, y, 50, 50);
+	ctx.fillRect(x2, y2, 50, 50);
 }
 
 function logic () {
-	if (x < game.width - width) requestAnimationFrame(drawThings);
+	requestAnimationFrame(drawThings);
 }
 
 requestAnimationFrame(drawThings);
