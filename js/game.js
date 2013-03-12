5975b091e5fa98986e083374818a58b00c1846c8
Shane Hudson <shane@shanehudson.net>
2013-03-12 11:06:21 +0000

    Put them together.

--- js/game.js
+++ js/game.js
@@ -1,45 +1,39 @@
-var game = document.querySelector('#game');
-var ctx = game.getContext('2d');
-game.height = height = (window.innerHeight - 10); 
-game.width = width = window.innerWidth;
-var objects = new Array();
-drawThings();
+var GameLogic = (function() {
+	var objects = new Array();
 
-function drawThings() {
-	ctx.clearRect(0, 0, width, height);
-	for (i=0; i<objects.length; i++) {
-		if (objects[i] != null) {
-			ctx.fillStyle = "#ff0000";
-			objects[i].x--;
-			ctx.fillRect(objects[i].x, objects[i].y, 50, 50);
-			if (objects[i].x < -50) {
-				delete objects[i];
+	function draw() {
+		for (i=0; i<objects.length; i++) {
+			if (objects[i] != null) {
+				ctx.fillStyle = "#ff0000";
+				objects[i].x--;
+				ctx.fillRect(objects[i].x, objects[i].y, 50, 50);
+				if (objects[i].x < -50) {
+					delete objects[i];
+				}
 			}
 		}
+		if (getRandomBool()) {
+			i = objects.length;
+			objects[i] = new Object();
+			objects[i].x = (canvas.width + 10);
+			objects[i].y = getRandomInt(0, canvas.height);
+		}	
 	}
-	if (getRandomBool()) {
-		i = objects.length;
-		objects[i] = new Object();
-		objects[i].x = (width + 10);
-		objects[i].y = getRandomInt(0, height);
-	}	
-}
 
-function logic () {
-	requestAnimationFrame(drawThings);
-}
-
-function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRandomBool() {
-	if (getRandomInt(0, (40000 /height)) == 1) {
-		return true;
-	} else {
-		return false;
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
-}
 
-requestAnimationFrame(drawThings);
-setInterval(logic, 1000/30);
+	function getRandomBool() {
+		if (getRandomInt(0, (40000 /canvas.height)) == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	return {
+		draw : draw
+	}
+
+});
--- js/init.js
+++ js/init.js
@@ -1,57 +1,64 @@
 // Pollyfill for RequestAnimationFrame
 (function() {
-  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-  window.requestAnimationFrame = requestAnimationFrame;
+	var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+	                          window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+	window.requestAnimationFrame = requestAnimationFrame;
+
 })();
 
 var userX, userY;
 var posX = 0;
 var posY = 0;
+
 var canvas = document.querySelector('#game');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 var ctx = canvas.getContext('2d');
+var game = new GameLogic();
 
-Leap.loop(function(frame, done) {
-	var pointer = frame.pointables[0];
-	if (frame.pointables.length > 0) {
-		for (var i = 0; i < frame.pointables.length; i++) {
-		    	var pointable = frame.pointables[i];
-		    	if (pointable.tipPosition[1] > pointer.tipPosition[1])
-		    		pointer = pointable;
-	    }
+var Init = (function() {
+
+	Leap.loop(function(frame, done) {
+		var pointer = frame.pointables[0];
+		if (frame.pointables.length > 0) {
+			for (var i = 0; i < frame.pointables.length; i++) {
+			    	var pointable = frame.pointables[i];
+			    	if (pointable.tipPosition[1] > pointer.tipPosition[1])
+			    		pointer = pointable;
+		    }
+		}
+		if (pointer != null)  {
+			//console.log(pointer.tipPosition);
+			userX = pointer.tipPosition[0];
+			userY = -pointer.tipPosition[1];
+		}
+		done(); // if you don't invoke this, you won't get more events
+	});
+
+	canvas.addEventListener('mousemove', function(e) {
+		userX = e.clientX;
+		userY = e.clientY;
+	});
+
+	function logic()  {
+		// LOGIC GOES HERE
+		posX = lerp(posX, userX, 0.5);
+		posY = lerp(posY, userY, 0.5);
+		requestAnimationFrame(render);
 	}
-	if (pointer != null)  {
-		//console.log(pointer.tipPosition);
-		userX = pointer.tipPosition[0];
-		userY = -pointer.tipPosition[1];
+
+	function render() {
+		// RENDERING GOES HERE
+	    ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.fillStyle = "#00FF00";
+		ctx.fillRect(posX + window.innerWidth/2, posY + window.innerHeight, 10, 10);
+		game.draw();
 	}
-	done(); // if you don't invoke this, you won't get more events
-});
-
-canvas.addEventListener('mousemove', function(e) {
-	userX = e.clientX;
-	userY = e.clientY;
-});
-
-function logic()  {
-	// LOGIC GOES HERE
-	posX = lerp(posX, userX, 0.5);
-	posY = lerp(posY, userY, 0.5);
-	requestAnimationFrame(render);
-}
-
-function render() {
-	// RENDERING GOES HERE
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = "#FF0000";
-	ctx.fillRect(posX + window.innerWidth/2, posY + window.innerHeight, 10, 10);
-}
 
 
-function lerp(start, end, speed) {
-	return start + (end - start) * speed;
-}
+	function lerp(start, end, speed) {
+		return start + (end - start) * speed;
+	}
 
-setInterval(logic, 1000/30);
+	setInterval(logic, 1000/30);
+})();
