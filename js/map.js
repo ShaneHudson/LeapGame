9acc27a841a7eee2a1cf0e202bf4257694b0667b
Chris Ewbank <djpc123@gmail.com>
2013-03-12 17:53:02 +0000

    even better asteroids

--- js/map.js
+++ js/map.js
@@ -10,7 +10,7 @@ var Map = (function() {
 		for (i=0; i<objects.length; i++) {
 			if (objects[i] != null) {
 				objects[i].x--;
-				ctx.drawImage(asteroids[objects[i].img], objects[i].x, objects[i].y, 120, 120);
+				ctx.drawImage(asteroids[objects[i].img], objects[i].x, objects[i].y, objects[i].size, objects[i].size);
 				if (objects[i].x < -128) {
 					delete objects[i];
 				}
@@ -22,13 +22,14 @@ var Map = (function() {
 			objects[i].x = canvas.width;
 			objects[i].y = getRandomInt(0, canvas.height);
 			objects[i].img = getRandomInt(0, 8);
+			objects[i].size = getRandomInt(50, 150);
 		}	
 	}
 	
 	function collide(x, y) {
 		for (i=0; i<objects.length; i++) {
 			if (objects[i] != null) {
-				if ((objects[i].x < x && (objects[i].x + 100) > x) && (objects[i].y < y && (objects[i].y + 100) > y)) {
+				if ((objects[i].x < x && (objects[i].x + objects[i].size) > x) && (objects[i].y < y && (objects[i].y + objects[i].size) > y)) {
 					return true;
 					break;
 				}
@@ -42,7 +43,7 @@ var Map = (function() {
 	}
 
 	function getRandomBool() {
-		if (getRandomInt(0, (60000 /canvas.height)) == 1) {
+		if (getRandomInt(0, (50000 /canvas.height)) == 1) {
 			return true;
 		} else {
 			return false;
